cb3178022dd5e774efe1a6577e329ffaa53f97b8
Daniel <danielfdalmau@gmail.com>
2016-02-07 00:33:10 -0500

    Reparando el problema del id del producto en detalle producto, y viendo lo del mar de codigo en el caso especial

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4])
+plt.ylabel('some numbers')
+plt.show()
 
   require_once 'includes/Customer.php';
   require_once 'includes/ShoppingCart.php';
